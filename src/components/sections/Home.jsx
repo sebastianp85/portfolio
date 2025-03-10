b9e3868a6c8f4ad269e8a9add3f50db0357f8644
Sebastian Petersson <erik.sebastian.petersson@gmail.com>
2025-03-10 11:26:45 +0100

    minor update

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -18,13 +18,13 @@ export const Home = () => {
             eager to learn and explore new opportunities.
           </p>
           <div className="flex justify-center space-x-4">
-            <a
+            {/* <a
               href="#projects"
               className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative 
             overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
             >
               View Projects
-            </a>
+            </a> */}
             <a
               href="#contact"
               className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all 
